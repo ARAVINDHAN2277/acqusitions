8000d24e171554d295993db627580a863ab4734a
Aravindhan <aravindhan2277@gmail.com>
2025-11-14 18:48:30 +0530

    Secure our API with Arcjet

--- src/validations/users.validation.js
+++ src/validations/users.validation.js
@@ -1,54 +1,25 @@
-import express from 'express';
-import logger from '#config/logger.js';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import authRoutes from '#routes/auth.routes.js';
-import securityMiddleware from '#middleware/security.middleware.js';
-import usersRoutes from '#routes/users.routes.js';
+import { z } from 'zod';
 
-const app = express();
+export const userIdSchema = z.object({
+    id: z
+        .string()
+        .regex(/^\d+$/, 'ID must be a valid number')
+        .transform(Number)
+        .refine(val => val > 0, 'ID must be a positive number'),
+});
 
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-app.use(
-    morgan('combined', {
-        stream: { write: message => logger.info(message.trim()) },
+export const updateUserSchema = z
+    .object({
+        name: z.string().min(2).max(255).trim().optional(),
+        email: z.email().max(255).toLowerCase().trim().optional(),
+        role: z.enum(['user', 'admin']).optional(),
     })
-);
-
-app.use(securityMiddleware);
-
-app.get('/', (req, res) => {
-    logger.info('Hello from Acquisitions!');
-
-    res.status(200).send('Hello from Acquisitions!');
-});
-
-app.get('/health', (req, res) => {
-    res
-        .status(200)
-        .json({
-            status: 'OK',
-            timestamp: new Date().toISOString(),
-            uptime: process.uptime(),
-        });
-});
-
-app.get('/api', (req, res) => {
-    res.status(200).json({ message: 'Acquisitions API is running!' });
-});
-
-app.use('/api/auth', authRoutes);
-app.use('/api/users', usersRoutes);
-
-app.use((req, res) => {
-    res.status(404).json({ error: 'Route not found' });
-});
-
-export default app;
+    .refine(
+        data => {
+            // Ensure at least one field is provided for update
+            return Object.keys(data).length > 0;
+        },
+        {
+            message: 'At least one field must be provided for update',
+        }
+    );
